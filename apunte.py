943b0f7f049fbe3876489e70334205aa5af7246f
Valeria Hidalgo <valeriahidalgo50@gmail.com>
2022-06-27 10:57:35 -0500

    apuntes

--- apunte.py
+++ apunte.py
@@ -12,3 +12,46 @@ import re
 
 consulta = input("Ingrese su consulta: ")
 validacion_consulta = re.match("^([A-Za-z]+[ ]{1}(AND|OR|NOT)[ ]{1}[A-Za-z]+([ ]{1}(AND|OR|NOT)[ ]{1}[A-Za-z]+)*)$|^[A-Za-z]+$",consulta)
+
+#---------Tuples y list
+#tuples --> unmutable, ordered
+#list --> mutable, ordered
+#En la tuples y list, puedo acceder a los items, así:
+items[0:4]
+items[::2] #selecciona todos los multiplos del 2, incluye el 0
+items[0:5:2] # selecciona del 0-4 y elige los multiplos del 2
+
+#añadir elementos a una lista
+lista = [1,2]
+lista.append("elemento") #añade solo uno a la vez lista = [1,2, "elemento"]
+lista.extends([2,3,2]) #añade más de uno a la vez lista = [1,2, "elemento",2,3,2]
+
+#las listas pueden concatenarse
+lista = lista+[0,1,2]
+
+#split convierte string en lista
+"hola soy vale".split() = ["hola","soy","vale"]
+
+#sort:ordenala lista
+a=[2,1,3]
+a.sort
+print(a) #1,2,3
+
+
+#ALIASING
+a = [1,2]
+b = a
+#todo cambio que haga en a, se reflejará en b y viceversa
+# esto pasa porque a y b referencian a  la misma lista
+# para hacer una copia o clonar se hace
+b = a[:] 
+#de esa manera se eliminan los problemas del aliasing
+
+#---------Sets
+#unordered: no recuerda la posición del elemento
+#no almacena elementos repetidos
+conjunto = {hola, 1,2,3}
+
+conjunto.add(4)
+conjunto.remove(1)
+
